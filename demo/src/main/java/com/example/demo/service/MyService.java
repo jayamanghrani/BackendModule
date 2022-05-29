089b2e03cb37d23c54bf8dbba050b1e216af9d4f
JayaOwner <jayamanghrani@gmail.com>
2022-05-29 15:04:18 +0530

    Spring boot demo project

---
+++ demo/src/main/java/com/example/demo/service/MyService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+	
+	public String myFunction()
+	{
+		
+		return "service called !!";
+		
+	}
+	
+}
